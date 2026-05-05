568ebf5c2103996f5aee8bc6ad2a42a7482c6106
amirhossein-k <amirhossein.k787878@gmail.com>
2026-05-05 22:37:22 +0330

    yhyhyhyhyh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,7 @@ import BaleMiniApp from "@/components/BaleMiniApp";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black text-white text-xl">
-      <BaleMiniApp>
-        <></>
-      </BaleMiniApp>
+      <BaleMiniApp />
     </div>
   );
 }
